cc228c50dd50bb8bde11e00a84a2042662de817c
Maxime <max.rondeau01@gmail.com>
2023-11-10 14:25:29 -0500

    Added comments in all code files

---
+++ app/utils/helpers.py
@@ -0,0 +1,14 @@
+'''AlmaxDesigner est une application web intuitive et extensible dédiée à la conception de balcons en 2D. L'application permet aux utilisateurs de dessiner des balcons en utilisant des formes géométriques prédéfinies ou à travers des outils de dessin manuel. Chaque élément du balcon peut être dimensionné précisément, avec une représentation à l'échelle dans le dessin 2D.
+
+## Fonctionnalités
+
+- Outils de dessin: Inclure des formes prédéfinies et des outils pour dessiner des lignes et des arcs.
+- Mesures précises: Permettre la saisie de dimensions spécifiques pour chaque côté des formes, reflétant ces mesures proportionnellement dans la vue 2D.
+- Points d'accrochage: Générer automatiquement des points de snapping pour faciliter l'alignement et la jonction des lignes.
+- Création de polygones: Assurer que les formes tracées soient fermées pour former des polygones.
+- Interface utilisateur conviviale: Concevoir une interface facile à utiliser et à comprendre pour les utilisateurs sans expérience de conception technique.
+- Architecture modulaire: Structurer le code de manière à permettre une expansion future, avec la possibilité d'ajouter de nouvelles fonctionnalités sans réécrire le code existant.
+
+## Technologies
+
+Aucune restriction sur le langage de programmation ou les bibliothèques; utilisez ce qui est le plus efficace et le plus moderne. L'application doit être compatible avec tous les navigateurs web modernes. Privilégier des solutions qui facilitent la maintenance et les mises à jour.'''
