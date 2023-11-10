19c9152778f354ff975be2eb07f2fe25f8ab98a3
Maxime <max.rondeau01@gmail.com>
2023-11-10 12:49:52 -0500

    Initial commit

---
+++ app/static/js/app.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (typeof fabric !== 'undefined') {
+        var canvas = new fabric.Canvas('balcony-designer');
+        // Ici, vous pouvez ajouter plus de logique pour gérer le dessin sur le canvas.
+    } else {
+    }
+});
